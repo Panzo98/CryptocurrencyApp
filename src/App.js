7151f203858641580ba92ff2992e1a7cf7aa5d0c
panzo98 <aleksandar.panzalovic1@apeiron-edu.eu>
2021-12-08 11:50:36 +0100

    Added Cryptocurrencies to homepage with search bar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Home,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route
+                exact
+                path="/exchanges"
+                element={<Exchanges></Exchanges>}
+              ></Route>
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies></Cryptocurrencies>}
+              ></Route>
+              <Route
+                exact
+                path="/crypto/:coinId"
+                element={<CryptoDetails></CryptoDetails>}
+              ></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rigths reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { buildQueries } from "@testing-library/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "f0b436d7d3msh0eda405fbc99029p152ce4jsn8dc419faafff",
+};
+const baseUrl = "https://coinranking1.p.rapidapi.com/";
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
+});
+export const { useGetCryptosQuery } = cryptoApi;
